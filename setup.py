007070e4325aad96712766152355dacb22464e9f
Trevin Miller <stumblinbear@gmail.com>
2018-01-22 10:30:40 -0600

    Removed setting

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 import os
 from distutils.core import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = 'epyphany',
@@ -21,7 +15,6 @@ setup(
     license = 'GNU',
     keywords = 'network discovery discover service broadcast packet',
     url = 'https://github.com/Secret-Web/epyphany',
-    long_description=read('README.md'),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: AsyncIO',
