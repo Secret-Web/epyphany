25854c04c617d0bcaf96f5c31e11f56464789446
Trevin Miller <stumblinbear@gmail.com>
2018-02-24 15:51:20 -0600

    Reuse address

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name = 'epyphany',
-    version = '0.1.0',
+    version = '0.1.1',
     packages=['epyphany'],
     description = ('A script for facilitating network service discovery.'),
     python_requires='>=3.5.3',
